e023a449df49a0ad44bba80cc13eaef9a37f2ece
lachanakumar95 <160472503+lachanakumar95@users.noreply.github.com>
2024-12-17 19:58:26 +0530

    Update google_passort.js

--- backend/config/google_passort.js
+++ backend/config/google_passort.js
@@ -59,7 +59,7 @@ passport.deserializeUser(async (id, done) => {
         {
           clientID: loginConfigData.google_clientId,
           clientSecret: loginConfigData.google_clientSecret,
-          callbackURL: '/google/callback',
+          callbackURL: '/api/google/callback',
         },
         async (accessToken, refreshToken, profile, done) => {
           try {
